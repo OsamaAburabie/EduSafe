a69d87f8b5903b05019c2737a98b45f5c60fb3e6
OsamaAburabie <aburabie.osama@gmail.com>
2022-04-07 09:31:33 +0300

    added onboarding screen with localstorage

--- App.js
+++ App.js
@@ -1,17 +1,31 @@
-import {Button, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+// In App.js in a new project
+
+import React, {useEffect} from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import OnboardingScreen from './src/screens/OnboardingScreen';
 import {useStorage} from './hooks/UseStorage';
 
-const App = () => {
-  const [token, setToken] = useStorage('token', 'default');
-  return (
-    <View>
-      <Text>{token}</Text>
-      <Button title="setname" onPress={() => setToken('osama')} />
-    </View>
+const Stack = createNativeStackNavigator();
+
+function App() {
+  const [appFirstLaunch, setAppFirstLaunch] = useStorage(
+    'appFirstLaunch',
+    true,
   );
-};
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        {appFirstLaunch ? (
+          <Stack.Screen name="OnboardingScreen" component={OnboardingScreen} />
+        ) : null}
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
-
-const styles = StyleSheet.create({});
