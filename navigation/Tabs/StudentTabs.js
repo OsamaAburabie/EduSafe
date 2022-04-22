3a1abc23dbcc71851c593d6c7aa945491fb811bf
OsamaAburabie <aburabie.osama@gmail.com>
2022-04-22 03:54:34 +0300

    added seen state for the invites

--- navigation/Tabs/StudentTabs.js
+++ navigation/Tabs/StudentTabs.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import HomeScreen from '../../src/screens/HomeScreen';
 import {EventStack, ProfileStack} from '../Stacks/TabStacks';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {tabScreenOptions} from '../../utils/tabScreenOptions';
+import {useMainContext} from '../../context/MainContextProvider';
 
 const Tab = createBottomTabNavigator();
 
 function StudentTabs() {
+  const {events} = useMainContext();
+  const [unSeen, setUnSeen] = React.useState(null);
+  useEffect(() => {
+    if (!events) return;
+    const unseen = events.filter(event => event.seen === false);
+    if (unseen.length > 0) {
+      setUnSeen(unseen.length);
+    } else {
+      setUnSeen(null);
+    }
+  }, [events]);
   return (
     <Tab.Navigator initialRouteName="Home" screenOptions={tabScreenOptions}>
       <Tab.Screen
@@ -38,6 +50,7 @@ function StudentTabs() {
           tabBarIcon: ({color}) => (
             <MaterialCommunityIcons name="bullhorn" color={color} size={26} />
           ),
+          tabBarBadge: unSeen,
         }}
       />
       <Tab.Screen
--- src/screens/EventScreen.js
+++ src/screens/EventScreen.js
@@ -1,11 +1,41 @@
 import {FlatList, ScrollView, StyleSheet, Text, View} from 'react-native';
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {COLORS} from '../../utils/colors';
 import EventItem from '../components/EventItem';
 import axios from '../../config/axios';
 import {useMainContext} from '../../context/MainContextProvider';
+import {useFocusEffect} from '@react-navigation/native';
 const EventScreen = () => {
-  const {events} = useMainContext();
+  const {user, events, setEvents} = useMainContext();
+
+  //update all events to seen when the screen is loaded
+  const updateSeenLocal = () => {
+    setEvents(
+      events.map(event => {
+        event.seen = true;
+        return event;
+      }),
+    );
+  };
+
+  const updateSeenRequest = async () => {
+    try {
+      axios.put(`/api/student/update_seen`, null, {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      updateSeenLocal();
+      updateSeenRequest();
+    }, []),
+  );
 
   return (
     <FlatList
