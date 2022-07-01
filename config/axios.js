523de23229c74c4e0ceae96f528973424967fa76
mahanad9090 <vcmndndj@gmail.com>
2022-07-02 01:09:23 +0300

    added prod url

--- config/axios.js
+++ config/axios.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
-import {storage} from '../context/MainContextProvider';
+import { storage } from '../context/MainContextProvider';
 const DEVURL = 'http://192.168.1.67:5000';
-const PRODURL = 'http://edusafe.ddns.net';
+const PRODURL = 'https://edu-safe.herokuapp.com';
 export const baseURL = PRODURL;
-const instance = axios.create({baseURL});
+const instance = axios.create({ baseURL });
 
-instance.interceptors.request.use(function (config) {
-  const token = storage.getString('token');
-  if (token && config.headers) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+instance.interceptors.request.use(function(config) {
+    const token = storage.getString('token');
+    if (token && config.headers) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
 });
 
 export default instance;
